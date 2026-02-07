5ab91abb2a673c244658b48ca436db6bafd4c518
shubhamavl <shubham.sinha@avl.com>
2026-02-07 12:12:38 +0530

    fix: remove obsolete migration file

--- server/db/migrations/000001_init_schema.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    user_id TEXT PRIMARY KEY,
-    name TEXT NOT NULL,
-    avatar TEXT,
-    last_seen DATETIME,
-    is_banned BOOLEAN DEFAULT 0,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
